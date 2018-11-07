1d67273356b5d7ea342e981d690e3a0f15a14676
abdallah <boudidib@live.com>
2018-11-07 12:08:29 +0100

    test

--- cvproject/src/app/app.module.ts
+++ cvproject/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { CvComponent } from './cv/cv.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {LayoutModule} from '@angular/cdk/layout';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule,MatCheckboxModule} from '@angular/material';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 @NgModule({
   declarations: [
     AppComponent,
     CvComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatIconModule,LayoutModule,OverlayModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
